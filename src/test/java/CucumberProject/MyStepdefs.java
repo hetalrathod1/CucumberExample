50a9f25c62c0b11d65388e0af90b257939efcf5f
hetalrathod1 <48393948+hetalrathod1@users.noreply.github.com>
2019-05-26 17:50:16 +0100

    Add files via upload

--- src/test/java/CucumberProject/MyStepdefs.java
+++ src/test/java/CucumberProject/MyStepdefs.java
@@ -1,9 +1,12 @@
 package CucumberProject;
 
+import cucumber.api.PendingException;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.By;
 
 public class MyStepdefs {
 
@@ -11,6 +14,7 @@ public class MyStepdefs {
     RegistrationPage registrationPage=new RegistrationPage();
     RegistrationSuccessPage registrationSuccessPage=new RegistrationSuccessPage();
     EuroCurrancy euroCurrancy=new EuroCurrancy();
+    LoginPage loginPage=new LoginPage();
 
     @Given("^user is on register page$")
     public void userIsOnRegisterPage()
@@ -47,5 +51,48 @@ public class MyStepdefs {
     public void userIsAbleToSeeProductInEuroCurrancy()
     {
         euroCurrancy.userSeeEuroCurrencyProduct();
+    }
+
+
+    @When("^user click on \"([^\"]*)\" link$")
+    public void userClickOnLink(String link) throws Throwable
+    {
+        homePage.clcikOnCatogaryLink(link);
+     }
+
+    @Then("^user should be able to navigate to all \"([^\"]*)\"   Successfully$")
+    public void userShouldBeAbleToNavigateToAllSuccessfully(String catogary) throws Throwable
+    {
+
+        Utils.assertURLtext(catogary);
+
+    }
+
+
+
+    @Given("^user click on loginpage$")
+    public void userClickOnLoginpage() {
+        homePage.clickOnLogin();
+    }
+
+
+    @When("^user enter  invalid \"([^\"]*)\" and/or \"([^\"]*)\"$")
+    public void userEnterInvalidAndOr(String emailid, String password) throws Throwable {
+        loginPage.userEnterLoginDetail(emailid,password);
+             }
+
+    @Then("^user should able to see\"([^\"]*)\"$")
+    public void userShouldAbleToSee(String errormessage) throws Throwable {
+        boolean b;
+      if (errormessage.contains("No customer account found")||errormessage.contains("The credentials provided are incorrect"))
+      {
+          b=true;
+      }
+      else
+      {
+          b=false;
+      }
+
+      Assert.assertTrue(b);
     }
 }
